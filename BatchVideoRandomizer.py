278143f4ed059edf80e541dddc8cef084d6aaf61
mystified131 <mystifiedthomas@gmail.com>
2019-06-21 03:34:38 -0500

    Tweaking

--- BatchVideoRandomizer.py
+++ BatchVideoRandomizer.py
@@ -1,4 +1,5 @@
 from moviepy.editor import *
+from moviepy.editor import VideoFileClip, concatenate_videoclips
 import os
 import random
 
@@ -51,6 +52,14 @@ def random_speed(clip):
     
     return new_clip
 
+def sequence(clip1, clip2, clip3, clip4):
+    
+    # here we set the clip.duration. It must be shorter than the duration of the video or it will throw an error
+
+    final_clip = concatenate_videoclips([clip1,clip2,clip3, clip4])
+
+    return final_clip
+
 directr = "Input"
 
 content = []
@@ -80,11 +89,7 @@ for mainlp in range(outnum):
     if dic1 > 5:
         clipb = clipa
 
-    dic2 = random.randrange(10)
-    if dic2< 6:
-        clipc = color_shift(clipb)
-    if dic2 > 5:
-        clipc = clipb
+    clipc = color_shift(clipb)
 
     dic3 = random.randrange(10)
     if dic3 < 6:
@@ -101,3 +106,47 @@ for mainlp in range(outnum):
     outstr = "Output\\" + str(mainlp) + "processed" + ".mp4"
 
     clipe.write_videofile(outstr)
+
+Conc = input("Would you like to randomly concatenate? (y/n): ")
+
+if Conc == "y":
+
+    directr = "Output"
+
+    content = []
+
+    for subdir, dirs, files in os.walk(directr):
+
+        for file in files:
+            filepath = subdir + os.sep + file
+
+            if filepath.endswith(".mp4"):
+                bline = str(filepath)
+                content.append(bline)
+
+    print(content)
+
+    ctr = len(content)
+
+    for x in range(ctr):
+        vidnum = random.randrange(ctr)
+        file1 = content[vidnum]
+        clip1 = VideoFileClip(file1)
+        vidnum = random.randrange(ctr)
+        file2 = content[vidnum]
+        clip2 = VideoFileClip(file2)
+        vidnum = random.randrange(ctr)
+        file3 = content[vidnum]
+        clip3 = VideoFileClip(file3)
+        vidnum = random.randrange(ctr)
+        file4 = content[vidnum]
+        clip4 = VideoFileClip(file4)
+
+
+        new_clip = sequence(clip1, clip2, clip3, clip4)
+
+        arg = str(ctr)
+
+        outstr = "Output\\" + str(x) + "prosseq" + arg + ".mp4"
+
+        new_clip.write_videofile(outstr)
